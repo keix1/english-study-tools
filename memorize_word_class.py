2ca232e0404f2bbdb680c5096ba3e73e110cb3cd
ke_ix1 <xkei4dev@gmail.com>
2019-10-18 15:40:24 +0900

    init

---
+++ memorize_word_class.py
@@ -0,0 +1,34 @@
+import random
+
+conjunction = ['although / though / even though', 'while / whereas', 'because / since', 'while', 'if / when / provided that', 'unless', 'even if', 'once', 'unless']
+
+preposition = ['despite / in spite of', 'because of', 'due to', 'during', 'according to', 'in addition to / besides, moreover / furthermore', 'instead of']
+
+adverb = ['nevertheless', 'therefore', 'even', 'instead']
+
+conjunction_dict = { v: 'c' for v in conjunction}
+preposition_dict = { v: 'p' for v in preposition}
+adverb_dict = { v: 'a' for v in adverb}
+
+question_dict = {}
+question_dict.update(conjunction_dict)
+question_dict.update(preposition_dict)
+question_dict.update(adverb_dict)
+
+while True:
+    print()
+    print('-----------', end='')
+    question_key = random.choice(list(question_dict.keys()))
+    print(question_key)
+    print()
+    word_class = input('この品詞は？> ')
+    if word_class == question_dict.get(question_key):
+        print('○ Correct!')
+    else:
+        print('☓ Incorrect')
+
+    print()
+    print(f'Correct Answer is {word_class}')
+
+    input('Input any key...')
+
