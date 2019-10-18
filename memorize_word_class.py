70ec7b983cafbf5b224b47166b8edbd64bc9f5ae
ke_ix1 <xkei4dev@gmail.com>
2019-10-18 15:51:42 +0900

    :art: UI: Easy to see

--- memorize_word_class.py
+++ memorize_word_class.py
@@ -17,18 +17,25 @@ question_dict.update(adverb_dict)
 
 while True:
     print()
-    print('-----------', end='')
+    print('##############')
+    print()
+    print('Choose from options:')
+    print('    "c": conjunction（接続詞）')
+    print('    "p": preposition（前置詞）')
+    print('    "a": adverb（副詞）')
+    print()
+
     question_key = random.choice(list(question_dict.keys()))
-    print(question_key)
+    print(f'-----------{question_key}')
     print()
     word_class = input('この品詞は？> ')
     if word_class == question_dict.get(question_key):
         print('○ Correct!')
     else:
         print('☓ Incorrect')
-
+        print()
+        print(f'Correct Answer is {word_class}')
     print()
-    print(f'Correct Answer is {word_class}')
 
     input('Input any key...')
 
